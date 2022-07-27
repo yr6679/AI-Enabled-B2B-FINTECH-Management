4ee64e42c6f797195c60c7b551ef34b0ca7dce93
Yash Rawat <61385376+yr6679@users.noreply.github.com>
2022-07-27 10:06:43 +0530

    Add files via upload

---
+++ Highradius_internship-main/Infinite Scroll/src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Table from "./Table.js";
+export default function App() {
+  return (
+    <div className="App">
+      <h1>1805164</h1>
+      <Table />
+    </div>
+  );
+}
---
+++ Highradius_internship-main/Milestone1/PojoHighradiusClass.java
@@ -0,0 +1,139 @@
+package com.Milestone1;
+import java.util.*;
+
+public class PojoHighradiusClass {
+	String business_code;
+	String cust_number; // too long
+	String name_customer;
+	Date clear_date;
+	int buisness_year;
+	String doc_id; // too long
+	Date posting_date;
+	int document_create_date; // too long
+	int document_create_date1; // too long
+	Date due_in_date; // too long
+	String invoice_currency;
+	String document_type;
+	int posting_id;
+	String area_business;
+	float total_open_amount;
+	int baseline_create_date;
+	String cust_payment_terms;
+	int invoice_id; // too long
+	int isOpen;
+	
+	public String getBusiness_code() {
+		return business_code;
+	}
+	public void setBusiness_code(String business_code) {
+		this.business_code = business_code;
+	}
+	public String getCust_number() {
+		return cust_number;
+	}
+	public void setCust_number(String cust_number) {
+		this.cust_number = cust_number;
+	}
+	public String getName_customer() {
+		return name_customer;
+	}
+	public void setName_customer(String name_customer) {
+		this.name_customer = name_customer;
+	}
+	public Date getClear_date() {
+		return clear_date;
+	}
+	public void setClear_date(Date clear_date) {
+		this.clear_date = clear_date;
+	}
+	public int getBuisness_year() {
+		return buisness_year;
+	}
+	public void setBuisness_year(int buisness_year) {
+		this.buisness_year = buisness_year;
+	}
+	public String getDoc_id() {
+		return doc_id;
+	}
+	public void setDoc_id(String doc_id) {
+		this.doc_id = doc_id;
+	}
+	public Date getPosting_date() {
+		return posting_date;
+	}
+	public void setPosting_date(Date posting_date) {
+		this.posting_date = posting_date;
+	}
+	public int getDocument_create_date() {
+		return document_create_date;
+	}
+	public void setDocument_create_date(int document_create_date) {
+		this.document_create_date = document_create_date;
+	}
+	public int getDocument_create_date1() {
+		return document_create_date1;
+	}
+	public void setDocument_create_date1(int document_create_date1) {
+		this.document_create_date1 = document_create_date1;
+	}
+	public Date getDue_in_date() {
+		return due_in_date;
+	}
+	public void setDue_in_date(Date due_in_date) {
+		this.due_in_date = due_in_date;
+	}
+	public String getInvoice_currency() {
+		return invoice_currency;
+	}
+	public void setInvoice_currency(String invoice_currency) {
+		this.invoice_currency = invoice_currency;
+	}
+	public String getDocument_type() {
+		return document_type;
+	}
+	public void setDocument_type(String document_type) {
+		this.document_type = document_type;
+	}
+	public int getPosting_id() {
+		return posting_id;
+	}
+	public void setPosting_id(int posting_id) {
+		this.posting_id = posting_id;
+	}
+	public String getArea_business() {
+		return area_business;
+	}
+	public void setArea_business(String area_business) {
+		this.area_business = area_business;
+	}
+	public float getTotal_open_amount() {
+		return total_open_amount;
+	}
+	public void setTotal_open_amount(float total_open_amount) {
+		this.total_open_amount = total_open_amount;
+	}
+	public int getBaseline_create_date() {
+		return baseline_create_date;
+	}
+	public void setBaseline_create_date(int baseline_create_date) {
+		this.baseline_create_date = baseline_create_date;
+	}
+	public String getCust_payment_terms() {
+		return cust_payment_terms;
+	}
+	public void setCust_payment_terms(String cust_payment_terms) {
+		this.cust_payment_terms = cust_payment_terms;
+	}
+	public int getInvoice_id() {
+		return invoice_id;
+	}
+	public void setInvoice_id(int invoice_id) {
+		this.invoice_id = invoice_id;
+	}
+	public int getIsOpen() {
+		return isOpen;
+	}
+	public void setIsOpen(int isOpen) {
+		this.isOpen = isOpen;
+	}
+}
